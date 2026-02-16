7ea2c995d0cbe9fef0c1eccb89896a718a051626
Kapchik43 <public.timur@gmail.com>
2026-02-16 13:40:08 +0400

    laba1

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	public class Book
+	{
+		private string _isbn;
+		private string _title;
+		private string _author;
+		private int _year;
+		private bool _status;
+
+		public string isbn
+		{
+			get => _isbn;
+			set => _isbn = value;
+		}
+		public string title
+		{
+			get => _title;
+			set => _title = value;
+		}
+
+		public string author
+		{
+			get => _author;
+			set => _author = value;
+		}
+
+		public int year
+		{
+			get => _year;
+			set => _year = value;
+		}
+		public bool status => _status;
+		public Book(string isbn, string title, string author, int year, bool status)
+		{
+			_isbn = isbn;
+			_title = title;
+			_author = author;
+			_year = year;
+			_status = status;
+		}
+		public bool Borrow()
+		{
+			if (_status)
+			{
+				_status = false;
+				return true;
+			}
+			return false;
+		}
+		public bool Return()
+		{
+			if (_status == false)
+			{
+				_status = true;
+				return true;
+			}
+			return false;
+		}
+		public void print()
+		{
+			Console.WriteLine($"isbn: {isbn}");
+			Console.WriteLine($"title: {title}");
+			Console.WriteLine($"author: {author}");
+			Console.WriteLine($"year: {year}");
+			Console.WriteLine($"status: {status}");
+			Console.WriteLine(new string('-', 25));
+		}
+	}
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp1;
+
+string isbn = "0121234324", title = "OOP", author = "Amankulov";
+int year = 2026;
+bool status = true;
+
+Book b = new Book(isbn, title, author, year, status);
+b.print();
+b.Borrow();
+b.print();
+b.Return();
+b.print();
